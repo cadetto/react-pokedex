c9da7b37abe361a5eca4a2c93b75a2d408787ad1
cadetto <cadettoc96@gmail.com>
2019-05-13 11:52:06 -0400

    Updates

---
+++ precache-manifest.1b7cc5d65841cfa70c0137171b4123dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ada6ffb473268bf7918f888eb92715",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "6ae2661190c314cc2530",
+    "url": "/react-pokedex/static/css/main.907c08eb.chunk.css"
+  },
+  {
+    "revision": "6af3727f9c44d39b6923",
+    "url": "/react-pokedex/static/js/2.53b41581.chunk.js"
+  },
+  {
+    "revision": "6ae2661190c314cc2530",
+    "url": "/react-pokedex/static/js/main.033bc8f9.chunk.js"
+  },
+  {
+    "revision": "49325ecd73a50f0ca547",
+    "url": "/react-pokedex/static/js/runtime~main.0848e284.js"
+  }
+]);
